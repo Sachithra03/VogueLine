872c90e8d53246ea2212d886a7234027548ac799
Sachithra Indrachapa <sachithrai1003@gmail.com>
2025-11-27 14:03:06 +0530

    Remove unused logo imports from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/NavBar'
 import Hero from './components/Hero'
 import Featured from './components/Featured'
