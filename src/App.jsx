3e59a22f8d931dedbab2999a5ae09f4251e3043f
Sachithra Indrachapa <sachithrai1003@gmail.com>
2025-11-27 14:16:10 +0530

    Fix import paths for components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Featured from './components/Featured'
-import About from './components/About'
-import Footer from './components/Footer'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Featured from '../components/Featured'
+import About from '../components/About'
+import Footer from '../components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
